b0ceb98f6accb3c47e143eb95ee5adcffdab12cc
Carlos <carlos.edusilva7899@gmail.com>
2023-11-27 18:51:30 -0300

    update repo

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace MVVM_API_SampleProject.Models
 {
-    public class Post
+    public class Todo
     {
-
+        public int UserId { get; set; }
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Body { get; set; }
-        public int UserId { get; set; }
-        
-        
+        public bool Completed { get; set; }
+
     }
 }
--- ViewModels/PostViewModel.cs
+++ ViewModels/PostViewModel.cs
@@ -1,25 +1,64 @@
-﻿using MVVM_API_SampleProject.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MVVM_API_SampleProject.Models;
+using MVVM_API_SampleProject.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MVVM_API_SampleProject.ViewModels
 {
-    public class PostViewModel
+    //Herdando ObservableObject do Community Toolkit Mvvm
+    internal partial class PostViewModel : ObservableObject, IDisposable
     {
-        public Post Post {  get; set; }
+        private readonly PostService _postService;
+
+        [ObservableProperty]
+        public int _UserId;
+        [ObservableProperty]
+        public int _Id;
+        [ObservableProperty]
+        public string _Title;
+        [ObservableProperty]
+        public string _Body;
+        //Uma coleção de Post
+        [ObservableProperty]
+        public ObservableCollection<Post> _posts;
+
 
         public PostViewModel()
         {
-            Post = new Post()
+            Posts = new ObservableCollection<Post>();
+            _postService = new PostService();
+
+        }
+
+        //Consumir a API Rest -> Criação dos Commands
+
+        public ICommand GetPostsCommand => new Command(async () => await LoadPostsAsync());
+
+        private async Task LoadPostsAsync()
+        {
+            try
             {
-                UserId = 1,
-                Id = 1,
-                Title = "Titulo",
-                Body = "Texto"
-            };
+                Posts = await _postService.GetAllPostsAsync();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção do service");
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
     }
 }
